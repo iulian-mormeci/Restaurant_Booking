29bd9c60cfef413a79b9bd74cac22db5d9e1a473
Iulian Catalin Mormeci <iulian.mormeci@proton.me>
2025-02-13 11:29:33 +0100

    started working on the header, added title of the website at the center of a slider. added swiper to the project. need to work on responsive

---
+++ resources/js/script.js
@@ -0,0 +1,15 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+let swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,27 @@
+<div class="container-fluid position-relative">
+    <div class="row justify-content-center">
+        <div class="title col-12 ">
+            <h1>Restaurant</h1>
+        </div>
+        <div class="swiper mySwiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                </div>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-pagination"></div>
+        </div>
+
+    </div>
+</div>
