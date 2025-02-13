7e3cf76881772ec4b5a0f7f079793713f7f67820
Iulian Catalin Mormeci <iulian.mormeci@proton.me>
2025-02-13 10:42:39 +0100

    navbar in progess, finished the layout, added links and removed dropdown. tested with phone view.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,14 +5,31 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{--title of the website inside of the tab of the browser--}}
     <title>Restaurant Booking</title>
+
+    {{--asset binding--}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
+
 <body>
 
+{{--component navbar--}}
+<x-nav></x-nav>
+
+{{--component header--}}
+<x-header></x-header>
+
+{{--div with container for the content of the page--}}
     <div class="min-vh-100">
         {{$slot}}
     </div>
 
+{{--component footer with website details--}}
+<x-footer></x-footer>
+
 </body>
+
 </html>
